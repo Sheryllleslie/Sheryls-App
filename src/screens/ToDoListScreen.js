d9736d22704135918decf119486c3ef893ba0481
Sheryl Leslie <107503443+Sheryllleslie@users.noreply.github.com>
2022-09-13 12:49:09 +0100

    Added an input on the to do list screen so the user can add items.

--- src/screens/ToDoListScreen.js
+++ src/screens/ToDoListScreen.js
@@ -1,14 +1,25 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import AppButton from '../components/MyButton.js';
+import { Input, Icon } from '@rneui/themed';
 
 
 export default function ToDoList() {
+  const [item, setItem] = useState("");
     return(
+    <>
     <View style={styles.container}>
       <Text style={styles.Bold}>To Do List!</Text>
+      <Text>{item}</Text>
       <StatusBar style="auto" />
     </View>
+        <Input
+      placeholder="Add item"
+      leftIcon={{ type: 'font-awesome', name: 'list' }}
+      onChangeText={(value) => setItem(value)}
+      />
+      </>
 )}
 
 
